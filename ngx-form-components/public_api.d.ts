d284fc9cf75e16b77aa4781794f4b2e1aacaab48
John <jonathan.braat@live.com>
2018-11-30 16:01:14 +0100

    Auto-generated commit

--- ngx-form-components/public_api.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/ngx-form-components.service';
-export * from './lib/ngx-form-components.component';
-export * from './lib/ngx-form-components.module';
-export * from './lib/range-input/range-input.component';
